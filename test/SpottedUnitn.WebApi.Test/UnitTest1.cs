f9354d2b6cfa008f69ac3be81a9ec11389242b78
Marco Luzzara <m.luzzara@aliaslab.net>
2020-08-22 11:06:10 +0200

    initial commit

---
+++ test/SpottedUnitn.WebApi.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SpottedUnitn.WebApi.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
